c1d957c9e2951d6aba43a9e1fe884628edcc25b2
sajinkpk980 <sajinkpk980@gmail.com>
2023-04-12 10:47:51 +0530

    First Commit -Engineer 1

---
+++ src/test/java/practice/Demo2Test.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Demo2Test {
+@Test
+public void sampleTest2() {
+	System.out.println("this is sample test 2");
+}
+public void sceneTest() {
+	System.out.println("this is scene test");
+}
+}
---
+++ src/test/java/practice/DemoTest.java
@@ -0,0 +1,10 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+@Test
+public void sampleTest() {
+	System.out.println("this is sample tesat");
+}
+}
