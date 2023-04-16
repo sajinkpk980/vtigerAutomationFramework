bd16344f68fc8446272dd8c7abae35e5d298dd1b
sajinkpk980 <sajinkpk980@gmail.com>
2023-04-16 19:49:37 +0530

    Engineer 1 - first change commit
    Change-Id: Ib2b98ce21d7dd9d614aaa41734c1adf638d9f6ee

--- src/test/java/practice/Demo2Test.java
+++ src/test/java/practice/Demo2Test.java
@@ -6,6 +6,7 @@ public class Demo2Test {
 @Test
 public void sampleTest2() {
 	System.out.println("this is sample test 2");
+	System.out.println("engineer 1 -commit");
 }
 public void sceneTest() {
 	System.out.println("this is scene test");
